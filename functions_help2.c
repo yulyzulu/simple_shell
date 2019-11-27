75a2e7c72e68aaef130a911185fa8f1ae44357fa
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-11-27 14:08:19 +0000

    Fix structure

--- functions_help2.c
+++ functions_help2.c
@@ -25,7 +25,6 @@ list_t *add_node(list_t **head, char *str)
 	}
 
 	new_node->str = _strdup(str);
-	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;
 
--- shell.h
+++ shell.h
@@ -14,19 +14,17 @@
 #include <fcntl.h>
 
 /**
-  * struct list_s - singly linked list
-  * @str: string - (malloc'ed string)
-  * @len: length of the string
-  * @next: points to the next node
-  *
-  * Description: singly linked list node structure
-  * for Holberton project
-  */
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 
 typedef struct list_s
 {
 	char *str;
-	unsigned int len;
 	struct list_s *next;
 } list_t;
 
