ae7a143eea73098e45d04a4ed85b266900522f37
Cristian Gomez <1154@holbertonschool.com>
2019-11-27 12:49:48 +0000

    Adding more functions and fixed add node

--- functions_help2.c
+++ functions_help2.c
@@ -7,7 +7,7 @@
 * Return: The New node.
 */
 
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node(list_t **head, char *str)
 {
 	int i = 0;
 	list_t *new_node;
--- shell.h
+++ shell.h
@@ -30,7 +30,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-list_t *add_node(list_t **head, const char *str);
+list_t *add_node(list_t **head, char *str);
 void free_list(list_t *head);
 void list_creator(char **paths, list_t **head);
 char *concatenate_list(list_t **head, char *cmd);
